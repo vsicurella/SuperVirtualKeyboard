bd053e0353b490d93d91eda907ac4d3690342e0d
Vincenzo Sicurella <vincenzo@New-MacBook-Pro-2018.local>
2019-05-15 14:07:48 -0400

    Made "MidiRemapper" struct

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -146,9 +146,6 @@ bool SuperVirtualKeyboardAudioProcessor::isBusesLayoutSupported (const BusesLayo
 
 void SuperVirtualKeyboardAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
 {
-    ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
 
 	if (externalKeyboardState)
 	{
@@ -163,26 +160,10 @@ void SuperVirtualKeyboardAudioProcessor::processBlock (AudioBuffer<float>& buffe
 	}
 
     // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+    // channels that didn't contain input data
+    for (auto i = getTotalNumInputChannels(); i < getTotalNumOutputChannels(); ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
 
 	midiMessages.addEvents(midiBuffer, 0, -1, 0);
 	midiBuffer.clear();
