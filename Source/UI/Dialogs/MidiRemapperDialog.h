ce37f8a70225546aab60c737e3b2c87d3b46ed81
vsicurella <sicurella12@gmail.com>
2019-05-24 19:00:48 -0400

    Added files bc I forgot!

---
+++ Source/UI/Dialogs/MidiRemapperDialog.h
@@ -0,0 +1,128 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.3
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include "../../../JuceLibraryCode/JuceHeader.h"
+
+#include "../../CommonFunctions.h"
+#include "../../PluginState.h"
+
+
+class MidiRemapTableModel : public TableListBoxModel
+{
+    int numRows = 3;
+
+    Array<int>* inputMap;
+    Array<int>* outputMap;
+
+public:
+
+    MidiRemapTableModel(Array<int>* inputMapIn, Array<int>* outputMapIn);
+
+    int getNumRows() override;
+
+    void paintRowBackground (Graphics&, int rowNumber, int width, int height, bool rowIsSelected) override;
+
+    void paintCell (Graphics&, int rowNumber, int columnId, int width, int height, bool rowIsSelected) override;
+
+    enum RowType
+    {
+        keyNumber = 1,
+        inputNote,
+        outputNote
+    };
+};
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class MidiRemapperDialog  : public Component,
+                            public ComboBox::Listener,
+                            public Slider::Listener,
+                            public Button::Listener
+{
+public:
+    //==============================================================================
+    MidiRemapperDialog (SuperVirtualKeyboardPluginState* pluginStateIn);
+    ~MidiRemapperDialog();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
+    void resized() override;
+    void comboBoxChanged (ComboBox* comboBoxThatHasChanged) override;
+    void sliderValueChanged (Slider* sliderThatWasMoved) override;
+    void buttonClicked (Button* buttonThatWasClicked) override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+
+    std::unique_ptr<TableHeaderComponent> remapTableHeader;
+    std::unique_ptr<MidiRemapTableModel> remapTableModel;
+    std::unique_ptr<TableListBox> remapTable;
+
+	int boxW = 10;
+	int boxH = 8;
+
+	SuperVirtualKeyboardPluginState* pluginState;
+	MidiRemapper* inputRemapper;
+	MidiRemapper* outputRemapper;
+
+    //[/UserVariables]
+
+    //==============================================================================
+    std::unique_ptr<ComboBox> comboBox;
+    std::unique_ptr<Label> midiInuptLable;
+    std::unique_ptr<Slider> slider;
+    std::unique_ptr<Label> noteRangeLabel;
+    std::unique_ptr<ToggleButton> manualMapToggle;
+    std::unique_ptr<ToggleButton> modeMapToggle;
+    std::unique_ptr<Viewport> noteViewport;
+    std::unique_ptr<ComboBox> modeLibraryBoxOG;
+    std::unique_ptr<Label> modeBoxOriginal;
+    std::unique_ptr<ComboBox> modeLibraryBoxOG2;
+    std::unique_ptr<Label> modeBoxRemap;
+    std::unique_ptr<Slider> modeOriginalRootSld;
+    std::unique_ptr<Slider> modeRemapRootSld;
+    std::unique_ptr<Label> rootNoteLabel;
+    std::unique_ptr<Viewport> degreesViewport;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MidiRemapperDialog)
+};
+
+//[EndFile] You can add extra defines here...
+
+//[/EndFile]
+
