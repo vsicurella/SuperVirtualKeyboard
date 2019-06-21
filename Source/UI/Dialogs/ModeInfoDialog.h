26334704b063baa8e33834d041f8951a46e099b4
vsicurella <sicurella12@gmail.com>
2019-06-21 13:57:35 -0400

    Created Mode Info Dialog

---
+++ Source/UI/Dialogs/ModeInfoDialog.h
@@ -0,0 +1,87 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.3
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include "../../../JuceLibraryCode/JuceHeader.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class ModeInfoDialog  : public Component,
+                        public Button::Listener,
+                        public Slider::Listener
+{
+public:
+    //==============================================================================
+    ModeInfoDialog ();
+    ~ModeInfoDialog();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
+    void resized() override;
+    void buttonClicked (Button* buttonThatWasClicked) override;
+    void sliderValueChanged (Slider* sliderThatWasMoved) override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+    std::unique_ptr<TextEditor> textEditor;
+    std::unique_ptr<TextEditor> textEditor2;
+    std::unique_ptr<TextEditor> textEditor3;
+    std::unique_ptr<Label> familyNameLbl;
+    std::unique_ptr<Label> stepsLbl;
+    std::unique_ptr<Label> intervalSizesLbl;
+    std::unique_ptr<Label> nameLabel;
+    std::unique_ptr<Label> modeSizeLbl;
+    std::unique_ptr<Label> scaleSizeLbl;
+    std::unique_ptr<ToggleButton> defaultNameBtn;
+    std::unique_ptr<Label> infoLbl;
+    std::unique_ptr<TextEditor> textEditor4;
+    std::unique_ptr<TextButton> applyButton;
+    std::unique_ptr<Slider> slider;
+    std::unique_ptr<Label> modeRotateLbl;
+    std::unique_ptr<Label> scaleSizeReadout;
+    std::unique_ptr<Label> modeSizeReadout;
+    std::unique_ptr<Label> intervalSizeReadout;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ModeInfoDialog)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
