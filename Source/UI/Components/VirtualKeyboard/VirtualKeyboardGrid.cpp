ae7819acb3b01be501f9b20a1de770c590aee8e3
vsicurella <sicurella12@gmail.com>
2019-10-20 15:39:30 -0400

    Work on key styles, but decided I should implement the grid differently so that the grid can refer to neighboring key data when resizing and placing keys.

--- Source/UI/Components/VirtualKeyboard/VirtualKeyboardGrid.cpp
+++ Source/UI/Components/VirtualKeyboard/VirtualKeyboardGrid.cpp
@@ -27,62 +27,79 @@ void KeyboardGrid::resizeKey(Key& key)
 	float spread = 4;
 	float height, width;
 	
-	if (key.order == 0)
-	{
-		height = 1;
-		width = 1;
-	}
-	else
-	{
-		switch (keyPlacement)
-		{
-			case(KeyPlacementType::nestedCenter):
-			{
+    if (key.order == 0)
+    {
+        height = 1;
+        width = 1;
+    }
+    else
+    {
+    switch (keyPlacement)
+    {
+        case(KeyPlacementType::nestedCenter):
+        {
+            //if (key.order > 0)
+            //{
                 float stepHeight = 0.55 + (key.step - 2) / 100.0f * spread;
-                height = stepHeight - stepHeight * (key.order - 1) / (key.step * 1.08);
-                width = 0.75 - (key.order - 1) * 0.1;
-				break;
-			}
+                height = stepHeight - stepHeight * (key.order - 1) / (key.step * 1.08f);
+                width = 0.75f - (key.order - 1) * 0.1f;
+            //}
+            break;
+        }
 
-			case(KeyPlacementType::flat):
-			{
-				float stepHeight = 0.55 + (key.step - 2) / 100.0f * spread;
-				height = stepHeight - stepHeight * (key.order - 1) / key.step;
-				width = 0.8;
-				break;
-			}
+        case(KeyPlacementType::flat):
+        {
+//            if (key.order > 0)
+//            {
+                float stepHeight = 0.55 + (key.step - 2) / 100.0f * spread;
+                height = stepHeight - stepHeight * (key.order - 1) / key.step;
+                width = 0.8f;
+//            }
+            break;
+        }
 
-			case(KeyPlacementType::adjacent):
-			{
+        case(KeyPlacementType::adjacent):
+        {
+            //if (key.order > 0)
+            //{
                 height = 0.55;
-                width = 1.0f / (key.step);
-				break;
-			}
+                width = 0.8f / (key.step-1); // shouldn't ever be 1 here
+//            }
+//            else
+//            {
+//                width += key.step > 1 * (key.step - 2) * (0.02f);
+//            }
+            break;
+        }
 
-			default: // 0 & 1, key nested right
-			{
-				float stepHeight = 0.55 + (key.step - 2) / 100.0f * spread;
-				height = stepHeight - stepHeight * (key.order - 1) / key.step;
-				width = 0.8 - (key.order - 1) * 0.1;
-			}
-		}
+        default: // 0 & 1, key nested right
+        {
+//            if (key.order > 0)
+//            {
+                float stepHeight = 0.55f + (key.step - 2) / 100.0f * spread;
+                height = stepHeight - stepHeight * (key.order - 1) / key.step;
+                width = 0.8f - (key.order - 1) * 0.1f;
+//            }
+            break;
+        }
 	}
-    
+    }
     key.setSize(width*getColumnWidth(), height*getRowHeight());
 }
 
 void KeyboardGrid::placeKey(Key& key)
 {
 	int xPosition = 0;
-
+    float column = getColumnWidth() + getColumnGap();
+    float halfColumn = column / 2.0f * (key.order > 0);
+    int colToPlace = ceil(key.modeDegree);
+    //DBG("halfcolumn="+String(halfColumn));
+    
 	switch (keyPlacement)
 	{
 		case(KeyPlacementType::nestedCenter):
 		{
-            int colToPlace = ceil(key.modeDegree);
-            float offset = (key.order > 0) * (getColumnWidth() / 2.0f);
-            offset = (int)(offset * 1.1);
-            xPosition = colToPlace * (getColumnWidth() + getColumnGap()) - offset;
+            xPosition = colToPlace * column - halfColumn;
             xPosition *= 1.0f + (0.001 * 10 * key.order > 1);
             key.setCentrePosition(xPosition, key.getHeight()/2.0f);
             break;
@@ -90,9 +107,11 @@ void KeyboardGrid::placeKey(Key& key)
 
 		case(KeyPlacementType::adjacent):
 		{
-            int colToPlace = (int)(key.modeDegree);
-            float offset = (key.modeDegree - colToPlace) * key.step > 2 * (key.step-1) * (key.getWidth() * 2);
-            xPosition = colToPlace * (getColumnWidth() + getColumnGap()) + offset;
+            int keyCol = (int) key.modeDegree;
+            xPosition = keyCol * column + halfColumn;
+            float stepOff = (key.order > 0 && key.step > 2) * (float)(key.order)/key.step;
+            DBG("KeyMD=" + String(key.modeDegree) + "\tStepOff=" + String(stepOff));
+            xPosition += stepOff * (key.getWidth() * column);
             key.setTopLeftPosition(xPosition, 0);
 
 			break;
@@ -100,12 +119,7 @@ void KeyboardGrid::placeKey(Key& key)
 
 		default: // 0 & 1, key nested right & flat
 		{
-			int colToPlace = ceil(key.modeDegree);
-			float offset = (key.order > 0) * (getColumnWidth() / 2.0f);
-
-			offset = (int)(offset * 1.2); // not sure why i have to do this to center the ordered keys
-
-			xPosition = colToPlace * (getColumnWidth() + getColumnGap()) - offset;
+            xPosition = colToPlace * column - halfColumn;
 
 			//DBG("keyNumber=" + String(key.keyNumber) + " modeDegree=" + String(key.modeDegree) + " colToPlace=" + String(colToPlace) + " offset=" + String(offset) + " x=" + String(xPosition));
 
@@ -113,6 +127,4 @@ void KeyboardGrid::placeKey(Key& key)
 			break;
 		}
 	}
-    
-    
 }
