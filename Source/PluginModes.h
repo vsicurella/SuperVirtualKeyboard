9bcf8049cf27e231558e04a6498c9717bd9dabe8
Vincenzo Sicurella <sicurella12@gmail.com>
2020-07-25 22:42:55 -0400

    Separated common plugin modes

---
+++ Source/PluginModes.h
@@ -0,0 +1,67 @@
+/*
+  ==============================================================================
+
+    PluginModes.h
+    Created: 25 Jul 2020 5:39:15pm
+    Author:  Vincenzo
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+namespace VirtualKeyboard
+{
+	enum UIMode
+	{
+		playMode = 0,
+		editMode,
+		mapMode
+	};
+
+	enum Orientation
+	{
+		horizontal = 0,
+		verticalLeft,
+		verticalRight
+	};
+
+	enum HighlightStyle
+	{
+		full = 1,
+		inside,
+		outline,
+		circles,
+		squares
+	};
+
+	enum VelocityStyle
+	{
+		linear = 1,
+		curved,
+		fixed
+	};
+
+	enum ScrollingStyle
+	{
+		smooth = 1,
+		stepped
+	};
+
+	enum KeyPlacementType
+	{
+		nestedRight = 1,
+		nestedCenter,
+		flat,
+		adjacent
+	};
+
+	enum KeyShapeFlags
+	{
+		Basic = 0x0001,
+		NotchPrevious = 0x0010,
+		NotchNext = 0x0100,
+		NotchCenter = 0x1000
+	};
+}
